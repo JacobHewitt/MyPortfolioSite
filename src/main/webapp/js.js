506101ec17ea6ed9ae7ea718b7f8c6dbc3a56428
jakeh <jakeh@10.128.5.193>
2017-01-25 12:36:37 +0000

    Ready to publish. Will probably change still

---
+++ src/main/webapp/js.js
@@ -0,0 +1,13 @@
+function visitGit(){
+    window.open("http://github.com/JacobHewitt", "_blank");
+    
+}
+
+function visitLink(){
+    window.open("https://www.linkedin.com/in/jacob-hewitt-323049103?trk=nav_responsive_tab_profile_pic", "_blank");
+    
+}
+
+function visitStack(){
+    window.open("http://stackoverflow.com/users/4356721/jake-hewitt", "_blank");
+}
